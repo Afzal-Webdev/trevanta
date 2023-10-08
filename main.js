d10efea55dec81fb128d89ce4fa10d162f111101
Afzal-Webdev <muhammad.afzal.sahir@gmail.com>
2023-10-08 19:57:06 +0500

    Main JS Added

---
+++ main.js
@@ -0,0 +1,12 @@
+
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+    document.body.style.cssText = "   overflow-y: hidden;"
+}
+
+/* Close */
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+    document.body.style.cssText = "   overflow-y: auto;"
+}
